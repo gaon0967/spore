e2ec22f6baad12b5d4f23b2aa0816cc652e7314d
Takch02 <lim70kr38@gmail.com>
2025-05-18 21:52:26 +0900

    Takch02 naverLogin test

---
+++ functions/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'off', // console.log 허용
+  },
+};
