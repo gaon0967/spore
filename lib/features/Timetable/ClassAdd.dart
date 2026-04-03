75a5dea855d629cc7c824ce843762eb89cbc7e62
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2026-04-03 20:24:51 +0900

    시간표 수정

--- lib/features/Timetable/ClassAdd.dart
+++ lib/features/Timetable/ClassAdd.dart
@@ -77,7 +77,7 @@ class _ClassAddState extends State<ClassAdd> {
     final double w = screenWidth * 0.00243;
     final double h = screenHeight * 0.001134;
 
-    final pickerHeight = 200.0*h;
+    final pickerHeight = screenHeight * 0.35;
 
     DateTime picked = DateTime(2024, 1, 1, initial.hour, initial.minute);
 
@@ -115,13 +115,13 @@ class _ClassAddState extends State<ClassAdd> {
                   ),
                 ),
                 CupertinoButton(
-                  padding: EdgeInsets.zero,
+                  padding: EdgeInsets.symmetric(horizontal: 24 * w, vertical: 12 * w),
                   child: Text(
                     '확인',
                     style: TextStyle(
                       fontFamily: 'Golos Text',
-                      fontSize: 15*w,
-                      fontWeight: FontWeight.w500,
+                      fontSize: 18 * w,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
                   onPressed: () {
@@ -131,6 +131,7 @@ class _ClassAddState extends State<ClassAdd> {
                     );
                   },
                 ),
+                SizedBox(height: MediaQuery.of(context).padding.bottom + screenHeight * 0.02),
               ],
             ),
           ),
